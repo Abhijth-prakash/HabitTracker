c57fcd22eae94dbbc5d5fd700b5b8e64ece3e60f
Abhijth-prakash <abhijithabs125@gmail.com>
2026-07-04 22:04:16 +0530

    added validation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,15 @@ import { useDispatch } from 'react-redux'
 import { addHabits } from '../redux/features/HabitSlice'
 import { useForm } from 'react-hook-form'
 import HabitCard from '../components/HabitCard'
-
+import { zodResolver } from '@hookform/resolvers/zod'
+import Schema from '../schema/HabitSchema'
 const Home = () => {
   const dispatch = useDispatch()
-  const {register,handleSubmit,formState} = useForm()
+  const {register,handleSubmit,formState} = useForm({
+    resolver:zodResolver(Schema)
+  })
+
+  const { errors } = formState
   
 const dataHandle = async (data) => {
   try {
@@ -21,6 +26,7 @@ const dataHandle = async (data) => {
 
     <form onSubmit={handleSubmit(dataHandle)}>
         <input type="text" {...register('habit')} placeholder='typesomething' />
+        <p>{errors?.habit?.message}</p>
         <button>+</button>
     </form>
     <HabitCard></HabitCard>
---
+++ frontend/src/schema/HabitSchema.js
@@ -0,0 +1,9 @@
+import z from "zod";
+
+const Schema = z.object({
+    habit: z.string.min(1,"habit is required")
+})
+
+
+
+export default Schema
