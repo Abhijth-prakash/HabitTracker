b02a9488a0fe94aa84852373b483bad45d948d09
Abhijth-prakash <abhijithabs125@gmail.com>
2026-07-04 19:40:34 +0530

    setup edit page

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -1,10 +1,25 @@
-import { useParams } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { useDispatch } from "react-redux"
+import { useNavigate, useParams } from "react-router-dom"
 
 
 const Edit = () => {
     const {id} = useParams()
+    const {register,handleSubmit} = useForm()
+    const dispatch =  useDispatch()
+    const navigate = useNavigate()
+    const dataHandle = async(data)=>{
+        const result = await dispatch()
+        if(!result.error){
+            navigate('/')
+        }
+    }
   return (
-    <div><p> this is habit{id}</p></div>
+    <>
+    <form onSubmit={handleSubmit(dataHandle)} > 
+        <input type="text" {...register('habit')} />
+        </form>    
+    </>
   )
 }
 
