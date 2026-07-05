3e82afaefec9a07df025811fde43890283a4d38b
Abhijth-prakash <abhijithabs125@gmail.com>
2026-07-05 18:01:56 +0530

    added error handling preset data

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -1,15 +1,29 @@
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
+import Schema from "../schema/HabitSchema"
 
 
 const Edit = () => {
+    const {habits} = useSelector(state=> state.data) 
     const {id} = useParams()
-    const {register,handleSubmit} = useForm()
+const habit = habits.find(item => item._id == id)
+
+const { register, handleSubmit, reset } = useForm({
+    resolver: zodResolver(Schema),
+    defaultValues: {
+        habit: habit?.habit
+    }
+})
     const dispatch =  useDispatch()
     const navigate = useNavigate()
     const dataHandle = async(data)=>{
-        const result = await dispatch()
+        const newdata ={
+            id:id,
+            habit: data
+        }
+        const result = await dispatch(newdata)
         if(!result.error){
             navigate('/')
         }
