80fe5de0e785f8b6d45a68eccb99961cdeb61403
Abhijth-prakash <abhijithabs125@gmail.com>
2026-07-31 19:38:06 +0530

    minor bugs

--- frontend/src/components/TodayCard.jsx
+++ frontend/src/components/TodayCard.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getLogs } from '../redux/features/HabitSlice'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getLogs } from "../redux/features/HabitSlice";
 
 const TodayCard = () => {
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch(getLogs())
-    },[])
+  const dispatch = useDispatch();
 
-    const {logs} = useSelector(state=> state.data)
-    const listItems = logs&& logs.map(item=> <li key={item._id}>
-        <span>{item.habit}</span>
-        <span>{item.completed}</span>
-        </li>)
+  useEffect(() => {
+    dispatch(getLogs());
+  }, [dispatch]);
+
+  const { logs } = useSelector((state) => state.data);
+
   return (
     <div>
-        <ol>{listItems}</ol>
+      <ol>
+        {logs?.map((item) => (
+          <li key={item._id}>
+            <span>{item.habit}</span>
+            <span>{item.completed ? " ✅ Completed" : " ⭕ Pending"}</span>
+          </li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default TodayCard
+export default TodayCard;
