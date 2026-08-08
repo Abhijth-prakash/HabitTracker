ec64750ea0e88ce791251da9a84b72cb6f511595
Abhijth-prakash <abhijithabs125@gmail.com>
2026-08-08 19:18:16 +0530

    creating login page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { Adduser } from '../redux/features/UserSlice'
 
 const Register = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate
 
   const {register,handleSubmit} = useForm()
 
-    const dataHandle = (data)=>{
-      
+    const dataHandle = async (data)=>{
+       const result  = await dispatch(Adduser(data))
+       if(!result.error){
+        console.log('pass')
+        navigate('/user/login')
+       }
   }
   return (
     <div><h1>this is register page</h1>
