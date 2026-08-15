c52ac4ea1129d37ed4656fdf68d839bb8b2bb517
Abhijth-prakash <abhijithabs125@gmail.com>
2026-08-15 13:41:40 +0530

    small changes

--- backend/controllers/Usercontroller.js
+++ backend/controllers/Usercontroller.js
@@ -44,7 +44,7 @@ const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -101,7 +101,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
